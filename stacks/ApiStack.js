843a3663d8459ddfea6c88e557317e64340f3774
Aaron Pennington <aaronpennington@Aarons-Mac-mini.lan>
2022-08-31 13:20:48 -0600

    Adding the API

---
+++ stacks/ApiStack.js
@@ -0,0 +1,35 @@
+import { Api, use } from "@serverless-stack/resources";
+import { StorageStack } from "./StorageStack";
+
+export function ApiStack({ stack, app }) {
+  const { table } = use(StorageStack);
+
+  // Create the API
+  const api = new Api(stack, "Api", {
+    defaults: {
+      function: {
+        permissions: [table],
+        environment: {
+          TABLE_NAME: table.tableName,
+        },
+      },
+    },
+    routes: {
+      "POST /premises": "functions/create.main",
+      "GET /premises/{id}": "functions/get.main",
+      "GET /premises": "functions/list.main",
+      "PUT /premises/{id}": "functions/update.main",
+      "DELETE /premises/{id}": "functions/delete.main",
+    },
+  });
+
+  // Show the API endpoint in the output
+  stack.addOutputs({
+    ApiEndpoint: api.url,
+  });
+
+  // Return the API resource
+  return {
+    api,
+  };
+}
